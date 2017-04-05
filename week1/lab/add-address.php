0f05517d6b380016ccd885afd67daa2e27c6dacd
Dobachesky <mgdobachesky@email.neit.edu>
2017-04-04 22:43:04 -0400

    remove redundancy from header in week one lab

--- week1/lab/add-address.php
+++ week1/lab/add-address.php
@@ -8,20 +8,7 @@
     </head>
     <body>
       <div class="container-fluid">
-        <div class="col-md-12">
-          <nav class="navbar navbar-default">
-            <div class="container-fluid">
-              <div class="navbar-header">
-                <a class="navbar-brand" href="./index.php">Michael Dobachesky - Lab 1</a>
-              </div>
-              <ul class="nav navbar-nav">
-                <li><a href="./index.php">Home</a></li>
-                <li><a href="./add-address.php">Add Address</a></li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-
+        <?php include './templates/header.html.php'; ?>
         <div class="col-md-10 col-md-offset-1">
         <?php
 
---
+++ week1/lab/templates/header.html.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="./index.php">Michael Dobachesky - Lab 1</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a href="./index.php">Home</a></li>
+        <li><a href="./add-address.php">Add Address</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
