645ca01b1214298b5a5d46f3c36885f353098f94
mike91doby <mike91doby@gmail.com>
2017-06-03 18:04:51 -0400

    git cache cleared

--- week2/demo/crud/ICrud.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface ICrud {
-    public function create();
-    public function read();
-    public function readAll();
-    public function update();
-    public function delete();
-}
